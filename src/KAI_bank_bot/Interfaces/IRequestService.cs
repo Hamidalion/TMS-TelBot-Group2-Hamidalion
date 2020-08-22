6497afa7b60559d648eb1978238b25af06fa7546
Hamidalion <66638336+Hamidalion@users.noreply.github.com>
2020-08-22 16:14:55 +0300

    feat: add services and remake aboutFiles

---
+++ src/KAI_bank_bot/Interfaces/IRequestService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using KAI_bank_bot.Models;
+
+namespace KAI_bank_bot.Interfaces
+{
+    /// <summary>
+    /// Сервис для отправки запросов.
+    /// </summary>
+    public interface IRequestService
+    {
+        /// <summary>
+        /// Получить валюту.
+        /// </summary>
+        /// <param name="code">Код валюты.</param>
+        /// <returns>Валюта.</returns>
+        Task<Currency> GetCurrencyAsync(string code);
+
+        /// <summary>
+        /// Получить список всех валют.
+        /// </summary>
+        /// <returns>Список всех валют.</returns>
+        Task<IEnumerable<Currency>> GetAllCurrenciesAsync();
+
+        /// <summary>
+        /// Получить курс валюты.
+        /// </summary>
+        /// <param name="code">Код валюты.</param>
+        /// <returns>Курс валюты.</returns>
+        Task<Rate> GetRateAsync(string code);
+
+        /// <summary>
+        /// Получить список всех курсов валют.
+        /// </summary>
+        /// <returns>Список курсов валют.</returns>
+        Task<IEnumerable<Rate>> GetAllRatesAsync();
+
+        /// <summary>
+        /// Получить курс валюты на определенную дату (как расширение)
+        /// </summary>
+        /// <param name="code">Код валюты.</param>
+        /// <param name="date">Дата.</param>
+        /// <returns>Курс валюты.</returns>
+        
+        Task<Rate> GetRateByDateAsync(string code, DateTime date);
+
+        /// <summary>
+        /// Получить список всех курсов валют на определенную дату.
+        /// </summary>
+        /// <param name="date">Дата.</param>
+        /// <returns>Список курсов валют.</returns>
+        Task<IEnumerable<Rate>> GetAllRatesByDateAsync(DateTime date);
+    }
+}
---
+++ src/KAI_bank_bot/Models/Rate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KAI_bank_bot.Models
+{
+    /// <summary>
+    /// Курс.
+    /// </summary>
+    public class Rate
+    {
+        public int Cur_ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Cur_Abbreviation { get; set; }
+        public int Cur_Scale { get; set; }
+        public string Cur_Name { get; set; }
+        public double Cur_OfficialRate { get; set; }
+    }
+}
